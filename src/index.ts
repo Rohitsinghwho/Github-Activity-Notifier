130aec5a64fc1f59d3274f73f761667b03bead37
rohitsinghwho <rohitsingh8655@gmail.com>
2026-04-10 15:34:12 +0530

    added:Add express.raw middleware which will keep the req body raw for time bein also add route for webhook

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
-
+import webhookRouter from "./routes/webhook.route";
 const app = express();
+
+
+app.use(express.raw({ type: "application/json" }));
+
+// apis
+app.use("/api/v1/webhook",webhookRouter);
 
 
 app.listen(3000,()=>{
