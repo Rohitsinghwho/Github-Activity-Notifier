a364ce07d54bb464b4bfd3353ceb9eeca7402272
rohitsinghwho <rohitsingh8655@gmail.com>
2026-04-10 15:19:24 +0530

    fix:add correct digest format and passed rawBody to hmac.update

--- src/utils/verifyWebHookSignature.ts
+++ src/utils/verifyWebHookSignature.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import crypto from "crypto";
 
-export default function verifyWebHookSignature(req:express.Request):boolean{
-    const signature=req.headers['x-hub-signature-256'] as string;
-    const secret=process.env.WEBHOOK_SECRET as string;
 
-    const hmac=crypto.createHmac('sha256',secret);
-    const digest='sha256'+hmac.update(JSON.stringify(req.body)).digest('hex');
+
+export default function verifyWebHookSignature(rawBody: Buffer, signature: string, secret: string): boolean {
+    const hmac = crypto.createHmac('sha256', secret);
+    hmac.update(rawBody);
+    const digest='sha256='+hmac.digest('hex');
     return crypto.timingSafeEqual(Buffer.from(signature),Buffer.from(digest));
 }
